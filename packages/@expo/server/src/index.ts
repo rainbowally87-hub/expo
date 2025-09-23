2cfcd99614788485c6f1eeb2dfa9c8a9bd56e866
Phil Pluckthun <phil@kitten.sh>
2025-09-23 12:43:37 +0100

    Add StatusError support

--- packages/@expo/server/src/index.ts
+++ packages/@expo/server/src/index.ts
@@ -3,5 +3,5 @@ export type ExpoRequest = Request;
 /** @deprecated Use Fetch API `Response` instead. */
 export type ExpoResponse = Request;
 
-export * from './runtime/api';
+export * from './runtime';
 export type * from './types';
